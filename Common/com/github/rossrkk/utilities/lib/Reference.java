b700c53096b6c3cd96853f11570f9bab74a612d6
RossRKK <rosskelso@btinternet.com>
2013-08-20 19:24:25 +0100

    changed the version number to 1.0.1

--- Common/com/github/rossrkk/utilities/lib/Reference.java
+++ Common/com/github/rossrkk/utilities/lib/Reference.java
@@ -3,7 +3,7 @@ package com.github.rossrkk.utilities.lib;
 public class Reference {
     public static final String MOD_ID = "UTIL";
     public static final String MOD_NAME = "Utilities";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String CHANNEL ="utilities";
 
     // EE3 References
