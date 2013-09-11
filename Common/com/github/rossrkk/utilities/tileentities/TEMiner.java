b047820d518328b6d7135b95b7c831e694823a11
RossRKK <rosskelso@btinternet.com>
2013-09-11 20:33:12 +0100

    Working on the miner a bit, not going to well :(

--- Common/com/github/rossrkk/utilities/tileentities/TEMiner.java
+++ Common/com/github/rossrkk/utilities/tileentities/TEMiner.java
@@ -7,6 +7,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
 public class TEMiner extends TileEntity implements IInventory, Power {
@@ -23,13 +24,25 @@ public class TEMiner extends TileEntity implements IInventory, Power {
 		tickCount++;
 		if (power >= 16 && tickCount == 10) {
 			tickCount = 0;
+			if (inventory[0].itemID == Item.pickaxeDiamond.itemID) {
+				worldObj.destroyBlock(xCoord, yCoord - 1, zCoord, true);
+			}
 		}
 	}
 	
 	@Override
 	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		
 		for (int i = 0; i < inventory.length; i++) {
-			inventory[i].readFromNBT(compound);
+			
+			NBTTagList items = compound.getTagList("Items");
+			if ((NBTTagCompound)items.tagAt(i) != null) {
+				NBTTagCompound item = (NBTTagCompound)items.tagAt(i);
+				int slot = item.getByte("Slot");
+				
+				setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(item));
+			}
 		}
 		
 		compound.getInteger("power");
@@ -37,8 +50,19 @@ public class TEMiner extends TileEntity implements IInventory, Power {
 	
 	@Override
 	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
 		for (int i = 0; i < inventory.length; i++) {
-			inventory[i].writeToNBT(compound);
+			NBTTagList items = new NBTTagList();
+				
+				ItemStack stack = getStackInSlot(i);
+				
+				if (stack != null) {
+					NBTTagCompound item = new NBTTagCompound();
+					stack.writeToNBT(item);
+					items.appendTag(item);
+				}
+			
+			compound.setTag("Items", items);
 		}
 		
 		compound.setInteger("power", power);
