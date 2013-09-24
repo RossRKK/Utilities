57208236d3b82b91df32c838ef0c658b989aa21c
RossRKK <rosskelso@btinternet.com>
2013-09-24 19:52:17 +0100

    Got the miner working

--- Common/com/github/rossrkk/utilities/block/BlockMiner.java
+++ Common/com/github/rossrkk/utilities/block/BlockMiner.java
@@ -64,6 +64,8 @@ public class BlockMiner extends BlockContainer{
 				}
 			}
 		}
+		
+		super.breakBlock(world, x, y, z, id, meta);
 	}
 	
 	
--- Common/com/github/rossrkk/utilities/tileentities/TEMiner.java
+++ Common/com/github/rossrkk/utilities/tileentities/TEMiner.java
@@ -1,8 +1,10 @@
 package com.github.rossrkk.utilities.tileentities;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
@@ -14,106 +16,136 @@ import net.minecraft.tileentity.TileEntity;
 import com.github.rossrkk.utilities.power.Power;
 
 public class TEMiner extends TileEntity implements IInventory, Power {
-	
+
 	ItemStack[] inventory = new ItemStack[10];
-	
+
 	int power;
 	public int maxPower = 1024;
-	
+	int tickCount = 0;
+
 	int heightDug = -1;
-	
+
 	@Override
 	public void updateEntity() {
-		try {
+		if (tickCount == 16) {
 			
-			//reset the dig height if the dig height is bellow the world
-			if (heightDug + yCoord < 1) {
-				heightDug = -1;
-			}
+			tickCount = 0;
 			
-			//if there is more than 16 power units run
-			if (power >= 16 && inventory[0] != null) {
-				if (inventory[0].itemID == Item.pickaxeDiamond.itemID) {
-					//if the block isn't bedrock
-					if (Block.blocksList[worldObj.getBlockId(xCoord, yCoord + heightDug, zCoord)].blockID != Block.bedrock.blockID) {
-						
-						//get the dropped block
-						ArrayList<ItemStack> dropped = Block.blocksList[worldObj.getBlockId(xCoord, yCoord + heightDug, zCoord)].getBlockDropped(worldObj, xCoord, yCoord + heightDug, zCoord, worldObj.getBlockMetadata(xCoord, yCoord + heightDug, zCoord), 1);
-						ItemStack[] droppedAr = dropped.toArray(new ItemStack[9]);
-						
-						//if the block is successfully destroyed
-						if (worldObj.destroyBlock(xCoord, yCoord + heightDug, zCoord, false)) {
-							for (int i = 0; i < dropped.size(); i++) {
-								//if the item is valid for the slot
-								if (isItemValidForSlot(i + 1, droppedAr[i])) {
-									//manual valid item check
-									if (getStackInSlot(i + 1).itemID == droppedAr[i].itemID) {
-										//if else to switch between the item adding system
-										if (getStackInSlot(i + 1).stackSize + droppedAr[i].stackSize >= droppedAr[i].getMaxStackSize() || getStackInSlot(i + 1) == null) {
-											droppedAr[i].stackSize = droppedAr[i].stackSize + getStackInSlot(i + 1).stackSize;
-											setInventorySlotContents(i + 1, droppedAr[i]);
-											
-											//decrease the power level
-											power -= 16;
-										} else {
-											droppedAr[i].stackSize = droppedAr[i].getMaxStackSize();
-											setInventorySlotContents(i + 1, droppedAr[i]);
-											
-											//decrease the power level
-											power -= 16;
+			try {
+				// reset the dig height if the dig height is bellow the world
+				if (heightDug + yCoord == 0) {
+					heightDug = -1;
+				}
+	
+				// if there is more than 16 power units run
+				if (power >= 16 && inventory[0] != null) {
+					if (inventory[0].itemID == Item.pickaxeDiamond.itemID) {
+						// if the block isn't bedrock
+						if (worldObj.getBlockId(xCoord, yCoord + heightDug, zCoord) != Block.bedrock.blockID) {
+							if (worldObj.getBlockId(xCoord, yCoord + heightDug,
+									zCoord) != 0) {
+								// get the dropped block
+								ArrayList<ItemStack> dropped = Block.blocksList[worldObj
+										.getBlockId(xCoord, yCoord + heightDug,
+												zCoord)].getBlockDropped(worldObj, xCoord, yCoord + heightDug, zCoord, worldObj.getBlockMetadata(xCoord, yCoord + heightDug, zCoord), 0);
+								ItemStack[] droppedAr = dropped.toArray(new ItemStack[9]);
+	
+								if (worldObj.destroyBlock(xCoord, yCoord + heightDug, zCoord, false)) {
+									for (int i = 0; i < dropped.size(); i++) {
+										for (int j = 1; j < inventory.length; j++) {
+											if (inventory[j] != null) {
+												if (inventory[j].itemID == droppedAr[i].itemID) {
+													if (inventory[j].stackSize + droppedAr[i].stackSize <= droppedAr[i].getMaxStackSize()) {
+														droppedAr[i].stackSize += inventory[j].stackSize;
+														setInventorySlotContents(j,
+																droppedAr[i]);
+														break;
+													} else {
+														droppedAr[i].stackSize = droppedAr[i].getMaxStackSize();
+														setInventorySlotContents(j, droppedAr[i]);
+														break;
+													}
+												}
+											} else {
+												setInventorySlotContents(j,
+														droppedAr[i]);
+												break;
+											}
+											onInventoryChanged();
 										}
 									}
+	
+									// damage the pickaxe
+									inventory[0].setItemDamage(inventory[0]
+											.getItemDamage() + 1);
 								}
 							}
-							//dig down
-							heightDug --;
-							//damage the pickaxe
-							inventory[0].setItemDamage(inventory[0].getItemDamage() + 1);
 						}
 					}
 				}
+				
+				//digDown
+				heightDug --;
+			} catch (InputMismatchException e) {
 			}
-		} catch (NullPointerException e) {
-			
 		}
+		tickCount ++;
 	}
-	
+
+	public void dropItem(ItemStack stack) {
+		float spawnX = xCoord + worldObj.rand.nextFloat();
+		float spawnY = yCoord + worldObj.rand.nextFloat();
+		float spawnZ = zCoord + worldObj.rand.nextFloat();
+
+		EntityItem droppedItem = new EntityItem(worldObj, spawnX, spawnY,
+				spawnZ, stack);
+
+		float mult = 0.05F;
+
+		droppedItem.motionX = (-0.5F + worldObj.rand.nextFloat()) * mult;
+		droppedItem.motionY = (4 + worldObj.rand.nextFloat()) * mult;
+		droppedItem.motionZ = (-0.5F + worldObj.rand.nextFloat()) * mult;
+
+		worldObj.spawnEntityInWorld(droppedItem);
+	}
+
 	@Override
 	public void readFromNBT(NBTTagCompound compound) {
 		super.readFromNBT(compound);
-		
+
 		for (int i = 0; i < inventory.length; i++) {
-			
+
 			NBTTagList items = compound.getTagList("Items");
-			if ((NBTTagCompound)items.tagAt(i) != null) {
-				NBTTagCompound item = (NBTTagCompound)items.tagAt(i);
+			if ((NBTTagCompound) items.tagAt(i) != null) {
+				NBTTagCompound item = (NBTTagCompound) items.tagAt(i);
 				int slot = item.getByte("Slot");
-				
-				setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(item));
+
+				setInventorySlotContents(slot,
+						ItemStack.loadItemStackFromNBT(item));
 			}
 		}
-		
+
 		power = compound.getInteger("power");
 		heightDug = compound.getInteger("heightDug");
 	}
-	
+
 	@Override
 	public void writeToNBT(NBTTagCompound compound) {
 		super.writeToNBT(compound);
 		for (int i = 0; i < inventory.length; i++) {
 			NBTTagList items = new NBTTagList();
-				
-				ItemStack stack = getStackInSlot(i);
-				
-				if (stack != null) {
-					NBTTagCompound item = new NBTTagCompound();
-					stack.writeToNBT(item);
-					items.appendTag(item);
-				}
-			
+
+			ItemStack stack = inventory[i];
+
+			if (stack != null) {
+				NBTTagCompound item = new NBTTagCompound();
+				stack.writeToNBT(item);
+				items.appendTag(item);
+			}
+
 			compound.setTag("Items", items);
 		}
-		
+
 		compound.setInteger("power", power);
 		compound.setInteger("heightDug", heightDug);
 	}
@@ -130,8 +162,8 @@ public class TEMiner extends TileEntity implements IInventory, Power {
 
 	@Override
 	public ItemStack decrStackSize(int i, int j) {
-		ItemStack itemstack = getStackInSlot(i);
-		
+		ItemStack itemstack = inventory[i];
+
 		if (itemstack != null) {
 			if (itemstack.stackSize <= j) {
 				setInventorySlotContents(i, null);
@@ -171,7 +203,8 @@ public class TEMiner extends TileEntity implements IInventory, Power {
 
 	@Override
 	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-		return entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) <= 64;
+		return entityplayer.getDistanceSq(xCoord + 0.5, yCoord + 0.5,
+				zCoord + 0.5) <= 64;
 	}
 
 	@Override
