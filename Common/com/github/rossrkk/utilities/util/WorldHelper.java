bf28df7968140a178da7fb20a67ce68ecf492dcb
RossRKK <rosskelso@btinternet.com>
2014-01-30 17:08:55 +0000

    Solar panel works through transparent blocks

--- Common/com/github/rossrkk/utilities/util/WorldHelper.java
+++ Common/com/github/rossrkk/utilities/util/WorldHelper.java
@@ -1,11 +1,12 @@
 package com.github.rossrkk.utilities.util;
 
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
 
 public class WorldHelper {
 	public static boolean canSeeSky(World world,int x, int y, int z) {
 		for (int i = y + 1; i <= 256; i ++) {
-			if(!(world.isAirBlock(x, i, z))) {
+			if(!(world.isAirBlock(x, i, z) || !Block.blocksList[world.getBlockId(x, i, z)].isOpaqueCube())) {
 				return false;
 			}
 		}
